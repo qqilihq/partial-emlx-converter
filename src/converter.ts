72f4546a5ffa5c24189aa02d0a782612ec628225
Christian Niessner <marvkis@users.noreply.github.com>
2025-02-09 19:59:25 +0100

    Feature: added `imapImport` mode

--- src/converter.ts
+++ src/converter.ts
@@ -1,10 +1,12 @@
 import * as fs from 'fs';
 import * as glob from 'glob';
+import * as stream from 'stream';
 import * as path from 'path';
 import * as ProgressBar from 'progress';
 import * as util from 'util';
 import * as commander from 'commander';
 import * as plist from 'plist';
+import * as imap from 'imapflow';
 
 // @ts-ignore
 import { Splitter, Joiner, Rewriter } from 'mailsplit';
@@ -20,24 +22,30 @@ class DeletedMessageError extends Error {
   }
 }
 
+async function setupEnv(inputDir: string) {
+  const files = await util.promisify(glob)('**/*.emlx', { cwd: inputDir });
+  const bar = new ProgressBar('Converting [:bar] :percent :etas :file', { total: files.length, width: 40 });
+  return { files, bar };
+}
+
 export async function processEmlxs(
   inputDir: string,
   outputDir: string,
   ignoreErrors?: boolean,
   skipDeleted?: boolean
 ): Promise<void> {
-  const files = await util.promisify(glob)('**/*.emlx', { cwd: inputDir });
-  const bar = new ProgressBar('Converting [:bar] :percent :etas :file', { total: files.length, width: 40 });
+  const { files, bar } = await setupEnv(inputDir);
   for (const file of files) {
     bar.tick({ file });
     const resultPath = path.join(outputDir, `${stripExtension(path.basename(file))}.eml`);
     try {
       const writeStream = fs.createWriteStream(resultPath);
-      const messages = await processEmlx(path.join(inputDir, file), writeStream, ignoreErrors, skipDeleted);
-      messages.forEach(message => bar.interrupt(`${file}: ${message}`));
+      const res = await processEmlx(path.join(inputDir, file), writeStream, ignoreErrors, skipDeleted);
+      res.messages.forEach(message => bar.interrupt(`${file}: ${message}`));
     } catch (e) {
       if (e instanceof DeletedMessageError && e.message == 'DELETED') {
         bar.interrupt(`${file}: Message is marked as deleted (skipped)`);
+        fs.unlinkSync(resultPath);
         continue;
       }
       bar.interrupt(
@@ -46,6 +54,101 @@ export async function processEmlxs(
       bar.terminate();
       throw e;
     }
+  }
+}
+
+export async function imapImport(
+  inputDir: string,
+  options: {
+    port: number;
+    ignoreErrors?: boolean;
+    skipDeleted?: boolean;
+    tls: 'yes' | 'no';
+    host: string;
+    user: string;
+    pass: string;
+    mailbox: string;
+  }
+): Promise<void> {
+  const conn = new imap.ImapFlow({
+    host: options.host,
+    port: options.port,
+    auth: {
+      user: options.user,
+      pass: options.pass
+    },
+    secure: options.tls == 'yes',
+    logger: false
+  });
+  await conn.connect();
+  const { files, bar } = await setupEnv(inputDir);
+  try {
+    for (const file of files) {
+      bar.tick({ file });
+      try {
+        let writeStream: stream.Writable;
+        const writeStreamCollector = new Promise<Buffer>(resolve => {
+          writeStream = new (class extends stream.Writable {
+            // _write(chunk: any, encoding: string, callback: (error?: Error | null) => void): void;
+            _buf: Buffer[] = [];
+            _write(chunk: Buffer, _encoding: string, callback: (error?: Error | null) => void): void {
+              this._buf.push(chunk);
+              callback();
+            }
+            _final(callback: (error?: Error | null) => void): void {
+              resolve(Buffer.concat(this._buf));
+              callback();
+            }
+          })();
+        });
+        const res = await processEmlx(
+          path.join(inputDir, file),
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          writeStream!,
+          options.ignoreErrors,
+          options.skipDeleted
+        );
+        res.messages.forEach(message => bar.interrupt(`${file}: ${message}`));
+        const msgData = await writeStreamCollector;
+        const dateRecvTS = res.plData['date-received'] as number | undefined;
+        let dateRecv: Date;
+        if (!dateRecvTS) {
+          throw new Error('no date-received in plist!'); // we could get 'Date' from message headers als fallback when this happens.
+        } else {
+          // Hint: this is the fix the timestamp to UTC
+          dateRecv = new Date(dateRecvTS * 1000);
+          const tzo = dateRecv.getTimezoneOffset();
+          dateRecv = new Date((dateRecvTS - tzo) * 1000);
+        }
+        // map emlx flags to imap flags
+        const imapFlags: string[] = [];
+        for (const flag of res.flags) {
+          if (flag == 'read') imapFlags.push('\\Seen');
+          else if (flag == 'answered') imapFlags.push('\\Answered');
+          else if (flag == 'deleted') imapFlags.push('\\Deleted');
+          else if (flag == 'draft') imapFlags.push('\\Draft');
+          else if (flag == 'flagged') imapFlags.push('\\Flagged');
+        }
+        await conn.append(options.mailbox, msgData, imapFlags, dateRecv);
+      } catch (e) {
+        if (e instanceof DeletedMessageError && e.message == 'DELETED') {
+          bar.interrupt(`${file}: Message is marked as deleted (skipped)`);
+          continue;
+        }
+        if (e instanceof Error) {
+          bar.interrupt(`Caught Error: ${e.message}`);
+          if (e.message.startsWith('Could not get attachment')) {
+            bar.interrupt(
+              `Encountered error when processing ${file} -- run with '--ignoreErrors' argument to avoid aborting the conversion.`
+            );
+          }
+        }
+        bar.terminate();
+        throw e;
+      }
+    }
+  } finally {
+    await conn.logout();
   }
 }
 
@@ -67,7 +170,7 @@ export async function processEmlx(
   resultStream: Writable,
   ignoreErrors = false,
   skipDeleted = false
-): Promise<string[]> {
+): Promise<{ messages: string[]; flags: EmlxFlags[]; plData: plist.PlistObject }> {
   const messages: string[] = [];
   // see here for a an example how to implement the Rewriter:
   // https://github.com/andris9/mailsplit/blob/master/examples/rewrite-html.js
@@ -81,6 +184,7 @@ export async function processEmlx(
       // no op (callback needs to be here though!)
     });
     data.decoder.on('end', () => {
+      // console.log(`\n\n${emlxFile} ${JSON.stringify(data.node.parentNode.headers.lines, null, 2)}\n\n`);
       integrateAttachment(emlxFile, data).catch(err => {
         // propagate error event
         if (ignoreErrors) {
@@ -93,15 +197,16 @@ export async function processEmlx(
       });
     });
   });
+  const emlxTransform = new SkipEmlxTransform(skipDeleted);
   await util.promisify(pipeline)(
     fs.createReadStream(emlxFile),
-    new SkipEmlxTransform(skipDeleted),
+    emlxTransform,
     new Splitter(),
     rewriter,
     new Joiner(),
     resultStream
   );
-  return messages;
+  return { messages: messages, flags: emlxTransform.flags, plData: emlxTransform.plData };
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -209,6 +314,8 @@ export class SkipEmlxTransform extends Transform {
   private bytesRead = 0;
   private skipDeleted: boolean;
   private plistChunks: Buffer[] = [];
+  public readonly flags: EmlxFlags[] = [];
+  public plData: plist.PlistObject = {};
 
   constructor(skipDeleted: boolean) {
     super();
@@ -253,16 +360,15 @@ export class SkipEmlxTransform extends Transform {
   _flush(callback: TransformCallback): void {
     // we parse & process the trailing plist data from the emlx file
     const plistDict = Buffer.concat(this.plistChunks).toString('utf8');
-    const plData = plist.parse(plistDict) as plist.PlistObject;
+    this.plData = plist.parse(plistDict) as plist.PlistObject;
 
     // the flags are documented here: https://docs.fileformat.com/email/emlx/
-    const flags = plData['flags'] as number;
-    const flagNames: EmlxFlags[] = [];
+    const flagsVal = this.plData['flags'] as number;
     let flagBit = 0;
     for (const flagName of EmlxFlagNames) {
       const mask = 1 << flagBit;
-      if (flags & mask) {
-        flagNames.push(flagName);
+      if (flagsVal & mask) {
+        this.flags.push(flagName);
       }
       if (flagBit == 9) {
         // flags jump from bit 9 to bit 23 (10-15: attachment count; 16-22: prio)
@@ -273,7 +379,7 @@ export class SkipEmlxTransform extends Transform {
     }
 
     // skip deleted messages
-    if (this.skipDeleted && flagNames.includes('deleted')) {
+    if (this.skipDeleted && this.flags.includes('deleted')) {
       callback(new DeletedMessageError('DELETED'));
     } else {
       callback();
@@ -294,6 +400,30 @@ export function processCli(): void {
     .argument('<output_directory>', 'output folder for .eml-files')
     .action((inputDir: string, outputDir: string, options: { ignoreErrors?: boolean; skipDeleted?: boolean }) => {
       processEmlxs(inputDir, outputDir, options.ignoreErrors, options.skipDeleted).catch(err => console.error(err));
+    });
+
+  program
+    .command('imapImport')
+    .description('Import mails from emlx to IMAP server')
+    .option('-p,--port <port_number>', 'IMAP port', parseInt, 993)
+    .requiredOption('-u,--user <username>', 'User for IMAP authentication')
+    .addOption(
+      new commander.Option('--pass <password>', 'Password for IMAP authentication')
+        .makeOptionMandatory(true)
+        .env('IMAP_PASS')
+    )
+    .requiredOption('-h,--host <hostname>', 'IMAP server hostname')
+    .requiredOption('-m,--mailbox <mailbox>', 'IMAP mailbox to import mails into', 'import')
+    .addOption(
+      new commander.Option('--tls <mode>', 'Use `no` to disable TLS')
+        .choices(['yes', 'no'])
+        .default('yes', 'tls enabled')
+    )
+    .option('--skipDeleted', 'Skip messages marked as deleted')
+    .option('--ignoreErrors', "Don't abort conversion on error (see the log output for details in this case)")
+    .argument('<input_directory>', 'input folder to read .emlx-files from')
+    .action((input: string, options) => {
+      imapImport(input, options).catch(err => console.error(err));
     });
 
   program.parse(process.argv);
